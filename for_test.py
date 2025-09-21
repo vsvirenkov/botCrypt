47334925ad1991654b27a954804f3b7d4da12a60
vsvirenkov <v.svirenkov@gmail.com>
2025-09-21 16:38:59 +0300

    Delete for_test.py

--- for_test.py
+++
@@ -1,3 +0,0 @@
-from pybit.unified_trading import HTTP
-session = HTTP(api_key="tBHpsOXb36gJofHkgF", api_secret="lazdaPLNjW78Jg6VWB6psa2FuQjLypKgDWlL", testnet=False)
-print(session.get_wallet_balance(accountType="UNIFIED", coin="USDT"))
